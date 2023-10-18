b2fa78d153731227c265c3cbb64cc75b8536120c
Yuri Iozzelli <y.iozzelli@gmail.com>
2023-10-18 21:34:59 +0200

    rework segments

--- src/main.rs
+++ src/main.rs
@@ -31,57 +31,61 @@ struct Velocity(Vec2);
 #[derive(Component, Clone)]
 struct Radius(f32);
 
-#[derive(Clone)]
-enum CurveSegment {
-    Line {
-        to: Vec2,
-    },
-    Circle {
-        center: Vec2,
-        radius: f32,
-        angle: f32,
-    }
+#[derive(Component)]
+struct Segment;
+
+#[derive(Component, Clone)]
+struct Line {
+    from: Vec2,
+    to: Vec2,
 }
-impl CurveSegment {
-    fn is_line(&self) -> bool {
-        match self {
-            CurveSegment::Line { .. } => true,
-            _ => false,
-        }
-    }
-    fn is_circle(&self) -> bool {
-        match self {
-            CurveSegment::Circle { .. } => true,
-            _ => false,
-        }
-    }
-    fn radius(&self) -> f32 {
-        match self {
-            CurveSegment::Circle { radius, .. } => *radius,
-            CurveSegment::Line { .. } => f32::INFINITY,
-        }
-    }
-    fn angle(&self) -> f32 {
-        match self {
-            CurveSegment::Circle { angle, .. } => *angle,
-            CurveSegment::Line { .. } => 0.0,
-        }
+
+impl Line {
+    fn to_path(&self) -> Path {
+        let mut builder = PathBuilder::new();
+        builder.move_to(self.from);
+        builder.line_to(self.to);
+        builder.build()
     }
 }
 
 #[derive(Component, Clone)]
-struct Curve {
-    head: Vec2,
-    path: Vec<CurveSegment>,
+struct Arc {
+    from: Vec2,
+    center: Vec2,
+    radius: f32,
+    angle: f32,
 }
-#[derive(Component)]
-struct CurvePath;
-#[derive(Component)]
-struct CurveHead;
+
+impl Arc {
+    fn to_path(&self) -> Path {
+        let mut builder = PathBuilder::new();
+        builder.move_to(self.from);
+        builder.arc(self.center, Vec2::new(self.radius, self.radius), self.angle, 0.0);
+        builder.build()
+    }
+}
+
+#[derive(Component, Clone)]
+struct Head {
+    pos: Vec2,
+    tail: Option<Entity>
+}
+
+impl Head {
+    fn to_path(&self) -> Path {
+        GeometryBuilder::build_as(&Circle {
+            radius: 10.,
+            center: self.pos,
+        })
+    }
+}
+
 
 const RADIUS: f32 = 10.0;
 const VEL: f32 = 20.0;
 const TIMESTEP: f32 = 0.100;
+const EPSILON: f32 = 0.00001;
 fn update_acceleration(keys: Res<Input<KeyCode>>, mut query: Query<(&mut Radius), With<Player>>) {
     let mut r  = query.get_single_mut().unwrap();
     if keys.pressed(KeyCode::Left) {
@@ -93,45 +97,32 @@ fn update_acceleration(keys: Res<Input<KeyCode>>, mut query: Query<(&mut Radius)
     }
 }
 
-fn collide_segments(p1: &Vec2, s1: &CurveSegment, p2: &mut Vec2, s2: &CurveSegment) -> bool {
-    return false;
-}
-
-fn collide_curves(c1: &Curve, c2: &Curve) -> bool {
-    if c1.path.len() == 0 || c2.path.len() == 0 {
-        return false;
-    }
-    let s1 = c1.path.first().unwrap();
-    let p1 = c1.head;
-    let mut p2 = c2.head;
-    for s2 in &c2.path {
-        if collide_segments(&p1, s1, &mut p2, s2) {
-            return true;
-        }
-    }
-    return false;
-}
-
-fn update_collisions(mut query: Query<&Curve>) {
+fn update_collisions(mut query: Query<&Segment>) {
     for c1 in query.iter() {
         for c2 in query.iter() {
-            if collide_curves(c1, c2) {
-                println!("collision!");
-            }
         }
     }
 }
 
-fn update_positions(mut query: Query<(&mut Position, &mut Velocity, &mut Curve, &Radius)>) {
-    for (mut p, mut v, mut c, r) in query.iter_mut() {
+fn update_positions(
+    mut query: Query<(&mut Position, &mut Velocity, &mut Head, &Radius, )>,
+    mut q_lines: Query<&mut Line>,
+    mut q_arcs: Query<&mut Arc>,
+    mut commands: Commands,
+) {
+    for (mut p, mut v, mut h, r,) in query.iter_mut() {
         let dt = TIMESTEP;
         if r.0.is_infinite() {
             *p = Position(p.0 + v.0*dt);
-            let head = c.head;
-            if c.path.len() == 0 || !c.path.last().unwrap().is_line() {
-                let new_seg = CurveSegment::Line { to: head };
-                c.path.push(new_seg);
+            if let Some(mut line) = h.tail.and_then(|t| q_lines.get_mut(t).ok()) {
+                line.to = p.0;
             } else {
+                h.tail = Some(commands.spawn((
+                    Line {
+                        from: h.pos,
+                        to: p.0,
+                    },
+                )).id());
             }
         } else {
             let radius_vec = -v.0.perp().normalize();
@@ -142,73 +133,62 @@ fn update_positions(mut query: Query<(&mut Position, &mut Velocity, &mut Curve, 
             let delta = (new_radius_vec - radius_vec)*r.0;
             *p = Position(p.0 + delta);
             *v = Velocity(new_radius_vec.perp()*v.0.length());
-            if c.path.len() == 0 || c.path.last().unwrap().radius() != r.0 || (c.path.last().unwrap().angle() - angle).abs() >= 2.0*PI {
-                c.path.push(CurveSegment::Circle { 
-                    center,
-                    radius: r.0.abs(),
-                    angle: -angle,
-                });
+            if let Some(mut arc) = h.tail.and_then(|t| q_arcs.get_mut(t).ok()).filter(|arc| (arc.center - center).length() < EPSILON)  {
+                arc.angle += angle;
             } else {
-                let last_angle  = match c.path.last().unwrap() {
-                    CurveSegment::Circle { angle, .. } => {
-                        angle
+                h.tail = Some(commands.spawn((
+                    Arc {
+                        from: h.pos,
+                        center,
+                        radius: r.0.abs(),
+                        angle,
                     },
-                    _ => unreachable!(),
-                };
-                *c.path.last_mut().unwrap() = CurveSegment::Circle {
-                    center,
-                    radius: r.0.abs(),
-                    angle: -angle + last_angle,
-                };
+                )).id());
             }
         }
-        c.head = p.0;
+        h.pos = p.0;
     }
 }
 
 fn update_paths_system(
-    q_curve: Query<(&Curve, &Children)>,
-    mut q_path: Query<(&CurvePath, &mut Path), Without<CurveHead>,>,
-    mut q_head: Query<(&CurveHead, &mut Path), Without<CurvePath>,>,
+    q_lines: Query<(Entity, &Line,), Changed<Line>>,
+    q_arcs: Query<(Entity, &Arc,), Changed<Arc>>,
+    q_heads: Query<(Entity, &Head,), Changed<Head>>,
+    mut commands: Commands,
 ) {
-    for (c, children) in q_curve.iter() {
-        for child in children.iter() {
-            if let Ok((_, mut p)) = q_head.get_mut(*child) {
-                *p = curve_to_head(c);
-            } else if let Ok((_, mut p)) = q_path.get_mut(*child) {
-                *p = curve_to_path(c);
-            }
-        }
-    }
-}
-
-fn curve_to_path(curve: &Curve) -> Path {
-    let mut builder = PathBuilder::new();
-    builder.move_to(curve.head);
-    for s in curve.path.iter().rev() {
-        match s {
-            CurveSegment::Line { to } => {
-                builder.line_to(*to);
+    for (e, l, ) in q_lines.iter() {
+        commands.entity(e).insert((
+            ShapeBundle {
+                path: l.to_path(),
+                ..default()
             },
-            CurveSegment::Circle { center, radius, angle } => {
-                builder.arc(*center, Vec2::new(*radius, *radius), *angle, 0.0);
-            },
-        }
+            Stroke::new(Color::BLACK, 3.0),
+        ));
     }
-    builder.build()
-}
-
-fn curve_to_head(curve: &Curve) -> Path {
-    GeometryBuilder::build_as(&Circle {
-        radius: 10.,
-        center: curve.head,
-    })
+    for (e, a, ) in q_arcs.iter() {
+        commands.entity(e).insert((
+            ShapeBundle {
+                path: a.to_path(),
+                ..default()
+            },
+            Stroke::new(Color::BLACK, 3.0),
+        ));
+    }
+    for (e, h, ) in q_heads.iter() {
+        commands.entity(e).insert((
+            ShapeBundle {
+                path: h.to_path(),
+                ..default()
+            },
+            Stroke::new(Color::BLACK, 3.0),
+        ));
+    }
 }
 
 fn setup_system(mut commands: Commands) {
-    let curve = Curve {
-        head: Vec2::ZERO,
-        path: vec![],
+    let head = Head {
+        pos: Vec2::ZERO,
+        tail: None,
     };
     commands.spawn(Camera2dBundle::default());
     commands.spawn((
@@ -216,27 +196,10 @@ fn setup_system(mut commands: Commands) {
         SpatialBundle {
             ..Default::default()
         },
-        curve.clone(),
         Player,
-        Position(curve.head),
+        Position(head.pos),
         Velocity(Vec2::new(VEL, 0.0)),
         Radius(f32::INFINITY),
-    )).with_children(|parent| {
-        parent.spawn((
-            ShapeBundle {
-                path: curve_to_path(&curve),
-                ..default()
-            },
-            Stroke::new(Color::BLACK, 3.0),
-            CurvePath,
-        ));
-        parent.spawn((
-            ShapeBundle {
-                path: curve_to_head(&curve),
-                ..default()
-            },
-            Stroke::new(Color::BLACK, 3.0),
-            CurveHead,
-        ));
-    });
+        head,
+    ));
 }
